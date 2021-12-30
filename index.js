09964ffe23c1eec2a5e1e9e55bd42ba5d120123c
nachogp23 <nachogo23@gmail.com>
2021-12-30 12:03:44 +0100

    Iteración #6: Bucles

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // // 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
 // let x = 50;
 
-// // 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+// // 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
 
 // let h = 5;
 // let y = 10;
@@ -21,11 +21,11 @@
 
 // character.age = 25;
 
-// // 1.2 Declara 3 variables con los nombres y valores siguientes 
-// // 	firstName = 'Jon'; 
-// // 	lastName = 'Snow'; 
-// // 	age = 24; 
-// // Muestralos por consola de esta forma: 
+// // 1.2 Declara 3 variables con los nombres y valores siguientes
+// // 	firstName = 'Jon';
+// // 	lastName = 'Snow';
+// // 	age = 24;
+// // Muestralos por consola de esta forma:
 // // 	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
 
 // const firsName = "Jon";
@@ -40,9 +40,8 @@
 
 // console.log(toy1.price + toy2.price);
 
-
-// 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
-// y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+// 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000
+// y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad
 // basePrice más el valor de la variable globalBasePrice.
 // let globalBasePrice = 10000;
 // let globalBasePrice = 25000;
@@ -66,7 +65,7 @@
 
 // alert(15 % 9);
 
-// // 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// // 1.4 Usa el correcto operador de asignación que resultará en x = 15,
 // // teniendo dos variables y = 10 y z = 5.
 // // const y = 10;
 // // const z = 5;
@@ -88,22 +87,18 @@
 // const myAvenger = avengers[0];
 // console.log(myAvenger);
 
-
 // 1.2 Cambia el primer elemento de avengers a "IRONMAN"
 
 // const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 
 // avengers[0] = "IRONMAN";
 
-
-
 // 1.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
 // const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 
 // alert(avengers.length);
 
-
-// 1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+// 1.4 Añade 2 elementos al array: "Morty" y "Summer".
 // Muestra en consola el último personaje del array
 // const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 
@@ -127,35 +122,64 @@
 
 //Iteración #5: Condicionales
 
-const number1 = 10;
-const number2 = 20;
-const number3 = 2;
+// const number1 = 10;
+// const number2 = 20;
+// const number3 = 2;
 
-// ejemplo
-if (number1 === 10) {
-    console.log('number1 es estrictamente igual a 10')
-}
+// // ejemplo
+// if (number1 === 10) {
+//     console.log('number1 es estrictamente igual a 10')
+// }
 
-if (number2 / number1 == 2) {
-    console.log("number2 dividido entre number1 es igual a 2");
-}
+// if (number2 / number1 == 2) {
+//     console.log("number2 dividido entre number1 es igual a 2");
+// }
 
-if (number1 !== number2) {
-    console.log("number1 es estrictamente distinto a number2");
-}
+// if (number1 !== number2) {
+//     console.log("number1 es estrictamente distinto a number2");
+// }
 
-if (number3 != number1) {
-    console.log("number3 es distinto number1");
-}
+// if (number3 != number1) {
+//     console.log("number3 es distinto number1");
+// }
 
-if (number3 * 5 == number1) {
-    console.log("number3 por 5 es igual a number1");
-}
+// if (number3 * 5 == number1) {
+//     console.log("number3 por 5 es igual a number1");
+// }
 
-if (number3 * 5 == number1 && number1 * 2 == number2) {
-    console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
-}
+// if (number3 * 5 == number1 && number1 * 2 == number2) {
+//     console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+// }
 
-if (number2 / 2 == number1 || number1 / 5 == number3) {
-    console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+// if (number2 / 2 == number1 || number1 / 5 == number3) {
+//     console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+// }
+
+//Iteración #6: Bucles
+// 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo
+// cuando el resto del numero dividido entre 2 sea 0.
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 1.3 Crea un bucle para conseguir dormir contando ovejas.
+// Este bucle empieza en 0 y termina en 10.
+// Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle
+// y cambia el mensaje en la última vuelta a 'Dormido!'.
+
+for (let i = 0; i < 11; i++) {
+    if (i == 10) {
+        console.log("Dornmido!");
+    } else {
+        console.log("Intentando dormir");
+    }
 }
